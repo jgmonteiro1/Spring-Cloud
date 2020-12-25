e330f3d818a407bdbaf2b48a619de2708767e631
jgmonteiro <joao.monteiro@bepay.me>
2020-12-24 21:24:22 -0300

    Implementação do microservice worker

---
+++ rh-worker/src/main/java/com/jgmonteiro/rhworker/resources/WorkerResource.java
@@ -0,0 +1,33 @@
+package com.jgmonteiro.rhworker.resources;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.jgmonteiro.rhworker.entities.Worker;
+import com.jgmonteiro.rhworker.repositories.WorkerRepository;
+
+@RestController
+@RequestMapping(value = "/workers")
+public class WorkerResource {
+	
+	@Autowired
+	private WorkerRepository repository;
+	
+	@GetMapping
+	public ResponseEntity<List<Worker>> getWorkers(){
+		List<Worker> workers = repository.findAll();
+		return ResponseEntity.ok().body(workers);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Worker> findById(@PathVariable Long id){
+		Worker obj = repository.findById(id).get();
+		return ResponseEntity.ok().body(obj);
+	}
+}
